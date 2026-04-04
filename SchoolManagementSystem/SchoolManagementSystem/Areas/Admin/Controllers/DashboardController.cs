b37c5d6b9bafcc99862b4b36be27669897ac776c
AnasMardood <enesmerdud26@gmail.com>
2026-04-04 21:35:35 +0300

    Apply .gitignore and clean tracked files

--- SchoolManagementSystem/SchoolManagementSystem/Areas/Admin/Controllers/DashboardController.cs
+++ SchoolManagementSystem/SchoolManagementSystem/Areas/Admin/Controllers/DashboardController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagement.DataAccess.Data;
 using SchoolManagement.DataAccess.Utilities;
+using System.Threading.Tasks;
 
 namespace SchoolManagementSystem.Areas.Admin.Controllers
 {
@@ -8,8 +11,20 @@ namespace SchoolManagementSystem.Areas.Admin.Controllers
     [Authorize(Roles = WebSiteRole.WebSite_Admin)]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.StudentCount = await _context.Students.CountAsync();
+            ViewBag.MaterialCount = await _context.Materials.CountAsync();
+            ViewBag.UserCount = await _context.ApplicationUsers.CountAsync();
+            ViewBag.AdvisorCount = await _context.Advisors.CountAsync();
+            
             return View();
         }
     }
